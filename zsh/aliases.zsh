f5f2dc6606727acf9babb2aff954a435e8310a13
Errol Cheong <escakot@gmail.com>
2024-02-07 19:03:26 -0500

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,10 +4,13 @@ alias cat='bat -P'
 alias emptycm='git commit -m "Empty commit" --allow-empty'
 alias ls="exa -lah"
 alias reload="source ~/.zshrc"
-alias skipci='git commit -m "Skip CI [skip ci]" --allow-empty'
 alias zshrc="vim ~/.zshrc"
 alias gitconfig="vim ~/.gitconfig"
 alias dotfiles="cd ~/.dotfiles"
 alias xcode="xcode-select -p"
 
+# GIT aliases
+alias gdn='git diff --name-only'
+alias gdcan='git diff --cached --name-only'
+alias skipci='git commit -m "Skip CI [skip ci]" --allow-empty'
 
